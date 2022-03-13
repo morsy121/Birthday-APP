292569217417269f1e56664605ecf3d840645038
ALNOUR <morsyhashish123@gmail.com>
2022-03-13 09:02:37 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BirthdayCard());
+}
+
+class BirthdayCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xffD2BCD5),
+        body: Center(
+          child: Image(
+            image: AssetImage('assets/images/happy.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
